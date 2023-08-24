2265fd8cd657e26723f7a3ed9d98ced3864fd681
Benjamin <bcarrasco@hbl.cl>
2023-08-24 19:18:31 -0400

    QR Test2

--- resources/views/arriendo/qr_reader.blade.php
+++ resources/views/arriendo/qr_reader.blade.php
@@ -1,27 +1,26 @@
 @extends('layouts.app-guest') 
 
 @section('content')
-    <div style="display: flex; justify-content: center; margin-top: 0;">
-        <div id="reader"></div>
-        <div id="show" style="display: none;">
-            <h4 style="color:crimson;">Redirigiendo a la página...</h4>
-            <p style="color: blue;" id="result"></p>
-        </div>
-    </div>
+<script src="./qrScript.js"></script>
+<div style="text-align: center;">
 
-    <script src="{{asset('assets/js/qrReader.js')}}"></script>
-    <script>
-        const html5Qrcode = new Html5Qrcode('reader');
-        const qrCodeSuccessCallback = (decodedText, decodedResult)=>{
-            if(decodedText){
-                document.getElementById('show').style.display = 'block';
-                //document.getElementById('result').textContent = decodedText;
-                html5Qrcode.stop();
-                window.location.href = decodedText; // Change this URL
-            }
+    <div id="reader" style="width: 500px;"></div>
+    <div id="show" style="display: none;">
+        <h4>Scanned Result</h4>
+        <p style="color: blue;" id="result"></p>
+    </div>
+</div>
+<script>
+    const html5Qrcode = new Html5Qrcode('reader');
+    const qrCodeSuccessCallback = (decodedText, decodedResult)=>{
+        if(decodedText){
+            document.getElementById('show').style.display = 'block';
+            document.getElementById('result').textContent = decodedText;
+            html5Qrcode.stop();
         }
-        const config = {fps:20, qrbox:{width:250, height:250}}
-        html5Qrcode.start({facingMode:"environment"}, config,qrCodeSuccessCallback );
-    </script>
+    }
+    const config = {fps:10, qrbox:{width:250, height:250}}
+    html5Qrcode.start({facingMode:"environment"}, config,qrCodeSuccessCallback );
+</script>
 
 @endsection('content')
