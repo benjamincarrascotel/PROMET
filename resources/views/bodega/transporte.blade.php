db198834fe9052c965d90d7cf9d5165e03fa12d7
bcarrasco <benjamincarrasco.tel@gmail.com>
2023-09-13 20:28:17 -0300

    Arreglos visuales

--- resources/views/bodega/transporte.blade.php
+++ resources/views/bodega/transporte.blade.php
@@ -63,108 +63,117 @@
                     -->
                 </div>
 
-                <!-- Row -->
-                <div class="row flex-lg-nowrap">
-                    <div class="col-12">
-                        <div class="row flex-lg-nowrap">
-                            <div class="col-12 mb-3">
-                                <div class="">
+                @if(count($arriendos))
+                    <!-- Row -->
+                    <div class="row flex-lg-nowrap">
+                        <div class="col-12">
+                            <div class="row flex-lg-nowrap">
+                                <div class="col-12 mb-3">
                                     <div class="">
-                                        <div class="row">
-                                            <div class="col-6 mb-4">
+                                        <div class="">
+                                            <div class="row">
+                                                <div class="col-6 mb-4">
 
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="row">
-                                            @foreach ($arriendos as $arriendo)
-                                            <div class="col-xl-3 col-lg-6">
-                                                <div class="card text-start user-contact-list">
-                                                    <div class="">
-                                                        <div class="card-header border-bottom text-white p-5" style="background: linear-gradient(135deg, rgb(241, 196, 111) 60%, rgb(214, 87, 2));">
-                                                            @if($arriendo->activo->foto)
-                                                                    <span class="avatar brround avatar-xxl d-block" style="background-image: url({{Storage::url('activos/'.$arriendo->activo->id."/".$arriendo->activo->foto)}})"></span>
-                                                                @else
-                                                                    <span class="avatar brround avatar-xxl d-block" style="background-image: url({{asset('assets/images/brand/favicon1.png')}})"></span>
-                                                                @endif 
-                                                            <div class=" ms-3 text-white">
-                                                                <p class="mb-0 mt-1 fs-18 font-weight-semibold">{{$arriendo->activo->marca}}<br>{{$arriendo->activo->modelo}}<br>{{$arriendo->activo->año}}</p>
+                                            <div class="row">
+                                                @foreach ($arriendos as $arriendo)
+                                                <div class="col-xl-3 col-lg-6">
+                                                    <div class="card text-start user-contact-list">
+                                                        <div class="">
+                                                            <div class="card-header border-bottom text-white p-5" style="background: linear-gradient(135deg, rgb(241, 196, 111) 60%, rgb(214, 87, 2));">
+                                                                @if($arriendo->activo->foto)
+                                                                        <span class="avatar brround avatar-xxl d-block" style="background-image: url({{Storage::url('activos/'.$arriendo->activo->id."/".$arriendo->activo->foto)}})"></span>
+                                                                    @else
+                                                                        <span class="avatar brround avatar-xxl d-block" style="background-image: url({{asset('assets/images/brand/favicon1.png')}})"></span>
+                                                                    @endif 
+                                                                <div class=" ms-3 text-white">
+                                                                    <p class="mb-0 mt-1 fs-18 font-weight-semibold">{{$arriendo->activo->marca}}<br>{{$arriendo->activo->modelo}}<br>{{$arriendo->activo->año}}</p>
 
-                                                                <small class="">ID ARRIENDO: {{$arriendo->id}}</small>
+                                                                    <small class="">ID ARRIENDO: {{$arriendo->id}}</small>
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                        <div class="p-5">
-                                                            <div class="wrapper">
-                                                                <p class="fs-14 font-weight-bold">ESTADO ARRIENDO :</p>
-                                                                <!-- State 1 -->
-                                                                @if($arriendo->estado == "BODEGA")
-                                                                <p class="mt-2 text-info ">BODEGA</p>
-                                                                @elseif($arriendo->estado == "EN CAMINO IDA")
-                                                                <!-- State 2 -->
-                                                                <p class="mt-2 text-info ">EN CAMINO IDA</p>
-                                                                @elseif($arriendo->estado == "EN CLIENTE")
-                                                                <!-- State 3 -->
-                                                                <p class="mt-2 text-info ">
-                                                                    EN CLIENTE 
-                                                                    @if($arriendo->activo->estado == "PARA RETIRO")
-                                                                    (PARA RETIRO)
-                                                                    @endif
-                                                                </p>
-                                                                @elseif($arriendo->estado == "EN CAMINO VUELTA")
-                                                                <!-- State 4 -->
-                                                                <p class="mt-2 text-info ">EN CAMINO VUELTA</p>
-                                                                @elseif($arriendo->estado == "BODEGA DE VUELTA")
-                                                                <!-- State 5 -->
-                                                                <p class="mt-2 text-info ">BODEGA DE VUELTA</p>
-                                                                @endif
-                                                            </div>
-
-                                                            <div class="wrapper">
-                                                                <p class="fs-14 font-weight-bold">Código Interno (Activo) :</p>
-                                                                <p class="mt-2 text-muted ">{{$arriendo->activo->codigo_interno}}</p>
-                                                            </div>
-
-                                                            <div class="wrapper">
-                                                                <p class="fs-14 font-weight-bold">Fecha Inicio :</p>
-                                                                <p class="mt-2 text-muted ">{{Carbon\Carbon::parse($arriendo->fecha_inicio)->format('d-m-Y')}}</p>
-                                                            </div>
-                                                            <div class="wrapper">
-                                                                <p class="fs-14 font-weight-bold">Fecha Término :</p>
-                                                                <p class="mt-2 text-muted ">{{Carbon\Carbon::parse($arriendo->fecha_termino)->format('d-m-Y')}}</p>
-                                                            </div>
-                                                            <div class="text-white text-center">
-                                                                <form method="GET" action="{{ route('arriendo.qr_reader', [$arriendo->activo->id]) }}">
-                                                                    <td class="align-middle">
-                                                                        <!-- State 1 -->
-                                                                        @if($arriendo->estado == "BODEGA")
-                                                                            <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
-                                                                        <!-- State 2 -->
-                                                                        @elseif($arriendo->estado == "EN CAMINO IDA")
-                                                                            <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
-                                                                        <!-- State 3 -->
-                                                                        @elseif($arriendo->estado == "EN CLIENTE" && $arriendo->activo->estado == "PARA RETIRO")
-                                                                            <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
-                                                                        <!-- State 4 -->
-                                                                        @elseif($arriendo->estado == "EN CAMINO VUELTA")
-                                                                            <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
-                                                                        @else
-                                                                            <button disabled class="btn btn-xl btn-warning me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">(ESPERANDO CONFIRMACIÓN)</button>
+                                                            <div class="p-5">
+                                                                <div class="wrapper">
+                                                                    <p class="fs-14 font-weight-bold">ESTADO ARRIENDO :</p>
+                                                                    <!-- State 1 -->
+                                                                    @if($arriendo->estado == "BODEGA")
+                                                                    <p class="mt-2 text-info ">BODEGA</p>
+                                                                    @elseif($arriendo->estado == "EN CAMINO IDA")
+                                                                    <!-- State 2 -->
+                                                                    <p class="mt-2 text-info ">EN CAMINO IDA</p>
+                                                                    @elseif($arriendo->estado == "EN CLIENTE")
+                                                                    <!-- State 3 -->
+                                                                    <p class="mt-2 text-info ">
+                                                                        EN CLIENTE 
+                                                                        @if($arriendo->activo->estado == "PARA RETIRO")
+                                                                        (PARA RETIRO)
                                                                         @endif
+                                                                    </p>
+                                                                    @elseif($arriendo->estado == "EN CAMINO VUELTA")
+                                                                    <!-- State 4 -->
+                                                                    <p class="mt-2 text-info ">EN CAMINO VUELTA</p>
+                                                                    @elseif($arriendo->estado == "BODEGA DE VUELTA")
+                                                                    <!-- State 5 -->
+                                                                    <p class="mt-2 text-info ">BODEGA DE VUELTA</p>
+                                                                    @endif
+                                                                </div>
 
-                                                                    </td>
-                                                                </form>
+                                                                <div class="wrapper">
+                                                                    <p class="fs-14 font-weight-bold">Código Interno (Activo) :</p>
+                                                                    <p class="mt-2 text-muted ">{{$arriendo->activo->codigo_interno}}</p>
+                                                                </div>
+
+                                                                <div class="wrapper">
+                                                                    <p class="fs-14 font-weight-bold">Fecha Inicio :</p>
+                                                                    <p class="mt-2 text-muted ">{{Carbon\Carbon::parse($arriendo->fecha_inicio)->format('d-m-Y')}}</p>
+                                                                </div>
+                                                                <div class="wrapper">
+                                                                    <p class="fs-14 font-weight-bold">Fecha Término :</p>
+                                                                    <p class="mt-2 text-muted ">{{Carbon\Carbon::parse($arriendo->fecha_termino)->format('d-m-Y')}}</p>
+                                                                </div>
+                                                                <div class="text-white text-center">
+                                                                    <form method="GET" action="{{ route('arriendo.qr_reader', [$arriendo->activo->id]) }}">
+                                                                        <td class="align-middle">
+                                                                            <!-- State 1 -->
+                                                                            @if($arriendo->estado == "BODEGA")
+                                                                                <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
+                                                                            <!-- State 2 -->
+                                                                            @elseif($arriendo->estado == "EN CAMINO IDA")
+                                                                                <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
+                                                                            <!-- State 3 -->
+                                                                            @elseif($arriendo->estado == "EN CLIENTE" && $arriendo->activo->estado == "PARA RETIRO")
+                                                                                <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
+                                                                            <!-- State 4 -->
+                                                                            @elseif($arriendo->estado == "EN CAMINO VUELTA")
+                                                                                <button class="btn btn-xl btn-success me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">Cambiar de fase</button>
+                                                                            @else
+                                                                                <button disabled class="btn btn-xl btn-warning me-2" type="submit" data-bs-toggle="" data-bs-target="#user-form-modal">(ESPERANDO CONFIRMACIÓN)</button>
+                                                                            @endif
+
+                                                                        </td>
+                                                                    </form>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </div>
+                                                @endforeach
                                             </div>
-                                            @endforeach
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <!-- Row -->
+                @else
+                    <div class="alert alert-warning">
+                        <ul>
+                            <h4 class="text-center mt-4"><b>NO EXISTEN ARRIENDOS ACTUALMENTE ACTIVOS</b></h4>
+                        </ul>
+                    </div>
+                @endif
 
             </div>
         </div>
@@ -175,10 +184,4 @@
 
     
 
-@endsection
-
-@section('scripts')
-    <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-    <link href="https://nightly.datatables.net/css/jquery.dataTables.css" rel="stylesheet" type="text/css" />
-    <script src="https://nightly.datatables.net/js/jquery.dataTables.js"></script>
 @endsection
